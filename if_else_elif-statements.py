9b11a5b4cabc1e3e0d996ac0ab0f3e4c32a73a36
Aaron Edward Glenn <53454869+AaronEdwardGlenn@users.noreply.github.com>
2020-04-21 22:51:08 -0700

    adding info on else and if else statements

---
+++ if_else_elif-statements.py
@@ -0,0 +1,23 @@
+if True:
+    print('Conditional was true')
+
+language = 'python'
+
+if language == 'python':
+    print('Conditional was true')
+
+# object identity uses 'is' instead of ==
+
+# else statement
+if language == 'java':
+    print('langage is python')
+else:
+    print('no match')
+
+# elif statement
+if language == 'python':
+    print('langage is python')
+elif language == 'java':
+    print('lang is java')
+else:
+    print('no match')
